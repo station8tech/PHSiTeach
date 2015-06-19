f9556504f9ce45cc7d7a928073e9341d328011d9
Sam Bush <samb@stat8.net>
2015-06-18 20:44:20 -0400

    update6

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
 			
 			var listHtml = "";
 			
-			var url = "http://www.zippod.net/RYP/jsonCurrent.php"
-			$.post(url, function(response){
+			var url = "http://www.stat8.net/iTeachTest/iTeacherDoc.cfc?method=getintList&returnformat=json"
+			$.get(url, function(response){
 			var json = $.parseJSON(response);
 			
 			
@@ -20,37 +20,118 @@ $(document).ready(function(){
 				
 			//this work for media	listHtml += "<li><a href='#' onclick=window.plugins.streamingMedia.playAudio('" + value.link + "')><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></li>";
 				
-			listHtml += "<li><a href='#' onclick=window.open('" + value.link + "','_blank','location=yes,toolbar=yes,allowInlineMediaPlayback=yes')><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></a><a href='#' onclick=window.open('http://www.stat8.net','_system')></a></li>";
+			//listHtml += "<li><a href='#' onclick=window.open('" + value.link + "','_blank','location=yes,toolbar=yes,allowInlineMediaPlayback=yes')><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></a><a href='#' onclick=window.open('http://www.stat8.net','_system')></a></li>";
 				
 				
 			//this works for inappbrowser 	listHtml += "<li><a href='#' onclick=window.open('" + value.link + "','_blank','location=yes,toolbar=yes,allowInlineMediaPlayback=yes')><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></li>";
 				
-				//listHtml += "<li><a href='#' onclick=window.plugins.ChildBrowser.showWebPage('http://www.google.com')><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></li>";
-				
-					//listHtml += "<li><a href='#' onclick='window.open(\"" +  value.link + "\");'><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></li>";
+		
 					
-					//listHtml += "<li><a href='" + value.link +"'><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></li>";
+					
+					listHtml += "<li data-id=" + value.id + "><a href='#'>" + value.lastname +  " , "+ value.firstname + "</a></li>";
 					
 				});//end each
 			
 			
-		$("#history").html(listHtml); 
-		$('ul').listview('refresh');
+		$("#teacherList").html(listHtml); 
+		$('ul').listview().listview('refresh');
 
-			});//end post podlist
+
+
+			});//end teacherlist
+			
+			
+		//captures teacher_id variable and moves to page2	
+	    var teacher_id;
+		$(document).on("click", "#teacherList >li", function() {
+			
+		teacher_id = $(this).closest("li").attr("data-id");
+		$.mobile.changePage($("#page2"));
+			//console.log(teacher_id);
+			});//click event for list
+			
+			//start of page2
+			
+			$(document).on('pagebeforeshow', '#page2', function() {  
+         var url = "http://www.stat8.net/iTeachTest/iTeacherDoc.cfc?method=getarch&returnformat=json"
+		 var classHtml = "";
+		 $.post(url,{ searchName: teacher_id } , function(response){
+			 var json = $.parseJSON(response);
+			 $.each(json, function(key, value){  
+			
+			classHtml += "<li data-id=" + value.id + "><a href='#'>" + value.test + "</a></li>";
+			 
+			 });//end each
+			 $("#classList").html(classHtml);
+			 $('ul').listview('refresh');
+			 
+		 });//end page 
+		 
+		
+     });// end of page 2 classlist
+     
+     //captures class_id variable and moves to page3	
+	    var class_id;
+		$(document).on("click", "#classList >li", function() {
+			
+		class_id = $(this).closest("li").attr("data-id");
+		$.mobile.changePage($("#page3"));
+			console.log(class_id);
+			});//click event for list
+
+	$(document).on('pagebeforeshow', '#page3', function() {  
+         var url = "http://www.stat8.net/jquery/iTeacherDoc.cfc?method=getcontent&returnformat=json"
+		 var contentHtml = "";
+		 $.post(url,{ searchName: class_id } , function(response){
+			 var json = $.parseJSON(response);
+			 $.each(json, function(key, value){  
+			
+			
+			contentHtml += "<li data-id=" + value.id + " data-content_id =" + value.content +"><a href='#'>" + value.Type + "</a></li>";
+			 
+			 });//end each
+			 $("#contentList").html(contentHtml);
+			 $('ul').listview('refresh');
+			 
+		 });//end page 
+		 
+		
+     });// end of page 3 contentlist
+     
+     
+     
+		 //captures class_id variable and content and moves to page4
+	    var content_id;
+		$(document).on("click", "#contentList >li", function() {
+			
+		content_id = $(this).closest("li").attr("data-content_id");
+		$.mobile.changePage($("#page4"));
+			console.log(class_id);
+			console.log(content_id);
+			});//click event for list	
+			
+			//start of page4
+	$(document).on('pagebeforeshow', '#page4', function() {  
+         var url = "http://www.stat8.net/jquery/iTeacherDoc.cfc?method=getdocs&returnformat=json"
+		 var documentHtml = "";
+		 $.post(url,{ searchName: class_id, content: content_id } , function(response){
+			 var json = $.parseJSON(response);
+			 $.each(json, function(key, value){  
+			
+			
+			documentHtml += "<li><a href='#' onclick=window.open('" + value.showlink + "','_blank','location=yes,toolbar=yes,allowInlineMediaPlayback=yes')>" + value.date + "&nbsp;&nbsp;-&nbsp;&nbsp;" + value.name + "</a><a href='#' onclick=window.open('" + value.showlink + "','_system')></a></li>";
+			 
+			 });//end each
+			 $("#documentList").html(documentHtml);
+			 $('ul').listview('refresh');
+			 
+		 });//end page 
+		 
+		
+     });// end of page 4 document list
+			
 			
 			
 			
 			
 	});//end document ready
-
-
-function openBrowser(link){ 
-				
-        cb = window.plugins.childBrowser;
-                console.log(link);
-                
-                cb.showWebPage(link); 
-            
-            } 
-	
